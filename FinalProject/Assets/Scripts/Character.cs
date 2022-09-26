a65a50d7973703ca439e67de60c0653e8fb18b89
Renaldo-Ren <58324247+Renaldo-Ren@users.noreply.github.com>
2022-09-26 19:09:23 +0700

    Upload the Game Project

---
+++ FinalProject/Assets/Scripts/Character.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    protected Animator anim;
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    protected Vector2 direction;
+    protected bool isAttacking = false;
+    //protected Coroutine attackCoroutine;
+    public bool isMoving
+    {
+        get
+        {
+            return direction.x != 0 || direction.y != 0;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        HandleLayers();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        //Frame Rate Independent
+        rb.velocity = direction.normalized * speed;
+
+        //Frame Rate Dependent
+        //transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    public void HandleLayers()
+    {
+        //If player is moving, then animate player movement
+        if (isMoving)
+        {
+            ActivateLayers("Walk_Layer");
+
+            //Sets the animation parameter so that he faces the correct direction
+            anim.SetFloat("x", direction.x);
+            anim.SetFloat("y", direction.y);
+        }
+        else if (isAttacking)
+        {
+            ActivateLayers("Attack_Layer");
+        }
+        else
+        {
+            //Else, back to idle animation
+            ActivateLayers("Idle_Layer");
+        }
+    }
+
+    public void ActivateLayers(string layername)
+    {
+        for (int i=0; i < anim.layerCount; i++)
+        {
+            anim.SetLayerWeight(i, 0);
+        }
+        anim.SetLayerWeight(anim.GetLayerIndex(layername), 1);
+    }
+
+    public void StopAttack()
+    {
+        //if(attackCoroutine != null)
+        //{
+            //StopCoroutine(attackCoroutine);
+            isAttacking = false;
+            anim.SetBool("attack", isAttacking);
+        //}
+    }
+}
